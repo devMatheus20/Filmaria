c854bc01feff4d573d4ec9ce9f7ab2604c3da399
Matheus Santos <matheusantos77@outlook.com>
2022-06-01 22:33:26 -0300

    header styles

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,19 +1,17 @@
 import React from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 import './styles.css'
 
 
 
 function Header () {
-
-    const navigate = useNavigate()
     
     return(
         <header>
             <Link to='/'><h1>Prime Filmes</h1></Link>
 
-            <button onClick={() => navigate('/favoritos')}>Filmes Salvos</button>
+            <Link to='/favoritos' className='favoritos'>Filmes Salvos</Link>
         </header>
     )
 }
